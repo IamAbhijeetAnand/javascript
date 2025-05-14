ac111c3e7e03f1a49e8ff825ebb565167a415f32
Abhijeet Anand <157940124+IamAbhijeetAnand@users.noreply.github.com>
2025-05-14 08:27:16 +0000

    Datatypes in Js

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all JS code as newer version
+
+alert(3 + 3)  //This will show error!, we are using nodejs, not browser
+
+console.log(3
+    +
+    3) //Code readability should be high
+
+console.log("Abhijeet")
+
+/* 
+Two most important documentation for JS
+1.tc39  -> Original documentation
+2.mdn  *
+ */
+
+let name = "Abhijeet"
+let age  = 18
+let isLoggedIn = false  
+
+
+//number  -> 2 to power 53
+//bigint                     //Used in trading, stockmarket etc. Where very large number is present
+//string  -> ""
+//boolean  -> true/false
+//null -> standalone value
+//undefined ->  it means value is not assigned now
+//symbol -> unique          //Figma tools use this extensively
+
+
+//object
