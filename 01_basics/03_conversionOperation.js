cb80142ff3ac833cc0d1830a398011593d206f4e
Abhijeet Anand <157940124+IamAbhijeetAnand@users.noreply.github.com>
2025-05-17 05:18:50 +0000

    String conversion confusion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -27,6 +27,44 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 let someNumber = 33;
 
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+//**********************************OPERATIONS*****************************
+
+let value = 3;
+let negValue = -value;
+
+// console.log(negValue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);  //power
+// console.log(2%2);
+
+let str1 = "Hello";
+let str2 = " Abhijeet";
+
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log(1 + "2");  // -> 12
+console.log("1" + 2);  // -> 12
+console.log(1 + 2);    // -> 3
+console.log("1" + "2");  // -> 12
+console.log("1" + 2 + 2);  // -> 122
+console.log(1 + 2 + "3");  // -> 33
+
+console.log(+true);  //Output -> 1
+console.log(+"");   //Output -> 0
+
+
+
+
+
+
+
+
 
