8565a90b77556789e81d439f60be30b2467c3bac
Abhijeet Anand <157940124+IamAbhijeetAnand@users.noreply.github.com>
2025-05-17 04:28:39 +0000

    Conversion of data types

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,9 +1,32 @@
-let score = "33abc";
+// let score = "33abc";
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
-let valueInNumber = Number(score);
+// let valueInNumber = Number(score);
 
-console.log(typeof valueInNumber);
-console.log(valueInNumber);   //This will give "NaN"
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber);   //This will give "NaN" also in case of undefined. If score = null then output will be 0
+
+// 33  -> Number
+//"33abc"  -> NaN
+// true -> 1; false -> 0
+
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+//console.log(booleanIsLoggedIn);
+
+//When value is 
+// 1 -> true;  0 -> false
+// "" -> false; 
+// "Abhijeet" -> true
+
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
